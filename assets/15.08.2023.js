2ff2ac62a5d2f0b225f9d9122f4bff64bf6fafb6
VAB\GiseleLuz <giseleluz@aguiabranca.com.br>
2023-09-03 22:57:53 -0300

    TDE funcao javascript utilizando map

--- assets/15.08.2023.js
+++ assets/15.08.2023.js
@@ -32,3 +32,15 @@ function chamada() {
     console.log(dados);
     resultado.innerHTML = ` Seu nome é: ${dados[0].nomeCliente}, seu email é : ${dados[0].emailCliente} e seu telefone é : ${dados[0].telefoneCliente}`;
   }
+
+  //trazendo  dados para lista de items
+
+  let ul = document.querySelector("#items");
+
+const dados = [
+{nome: "João", idade: 30},{nome:"Gisele", idade: 26}, {nome: "Luiza", idade: 31}
+]
+const listaElementos = dados.map(dado => `<li> Nome: ${dado.nome}, Idade: ${dado.idade}</li>`);
+
+ul.innerHTML = listaElementos.join('')
+
